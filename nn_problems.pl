e365786366f380d400ab73940f3bb0907c3a2ae2
Alejandro Do Nascimento <alejandrodnm@gmail.com>
2018-03-07 20:25:57 +0100

    Add 08

--- nn_problems.pl
+++ nn_problems.pl
@@ -76,3 +76,15 @@ my_flatten_2(X, Y) :- my_flatten_acc(X, Y, []).
 my_flatten_solution(X,[X]) :- \+ is_list(X).
 my_flatten_solution([],[]).
 my_flatten_solution([X|Xs],Zs) :- my_flatten(X,Y), my_flatten(Xs,Ys), append(Y,Ys,Zs).
+
+% P08 (**) Eliminate consecutive duplicates of list elements.
+%     If a list contains repeated elements they should be replaced with a single copy of the element. The order of the elements should not be changed.
+
+%     Example:
+%     ?- compress([a,a,a,a,b,c,c,a,a,d,e,e,e,e],X).
+%     X = [a,b,c,a,d,e]
+
+compress([], []).
+compress([X], [X]).
+compress([X, X|T], Z) :- compress([X | T], Z).
+compress([X, Y|T], [X|Z]) :- compress([Y | T], Z).
