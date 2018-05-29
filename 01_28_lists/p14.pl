5d9820adbdd3b8407b1e17d7d1ae1acc53470ac4
Alejandro Do Nascimento <alejandrodnm@gmail.com>
2018-05-29 22:12:48 +0200

    problem 14

---
+++ 01_28_lists/p14.pl
@@ -0,0 +1,7 @@
+% 1.14 (*) Duplicate the elements of a list.
+%     Example:
+%     ?- dupli([a,b,c,c,d],X).
+%     X = [a,a,b,b,c,c,c,c,d,d]
+
+dupli([], []).
+dupli([H|T], [H, H |X]) :- dupli(T, X).
